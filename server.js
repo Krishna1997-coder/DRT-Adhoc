cbb704fa0fc7836c451a7ef1e9dcddf59d07f6b1
Krishna1997-coder <pillutlamurali12@gmail.com>
2024-09-28 00:01:17 +0530

    Update server.js

--- server.js
+++ server.js
@@ -52,6 +52,6 @@ app.get('/viewAdhocs', (req, res) => {
 });
 
 // Start the server
-app.listen(PORT, () => {
+app.listen(PORT,'0.0.0.0', () => {
     console.log(`Server is running at http://localhost:${PORT}`);
 });
